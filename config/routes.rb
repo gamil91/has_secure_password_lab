a593f41f0d9a82bd51b07d1c87c2c24acca9f497
gamil91 <alexis_gamil99@yahoo.com.com>
2020-11-06 19:21:20 -0800

    commit tree f3d05b3bc18d9e6dbcfeb423584e460d83f2ab8e

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users
+  get '/welcome', to: 'users#index', as: 'welcome'
+  get '/login', to: 'sessions#new', as: 'login'
+  post '/sessions', to: 'sessions#create', as: 'sessions'
+
 end
+
